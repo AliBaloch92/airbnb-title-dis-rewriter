6da3f9902b6444584b07db45aaf60cdfbe6e5087
AliBaloch92 <95591924+AliBaloch92@users.noreply.github.com>
2023-07-20 20:43:56 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, jsonify
 import openai
 
-# Replace with yourOpenAI API key
+# Replace with your OpenAI API key
 openai.api_key = " your api key"
 
 app = Flask(__name__)
